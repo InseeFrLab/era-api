f21ee0568b88d4cffa676a053fcab498c40afecf
Mathieu Bergonzini <mathieu.bergonzini@insee.fr>
2023-09-06 10:21:38 +0200

    Update BusinessConstant.java

--- src/main/java/fr/insee/era/extraction_rp_famille/model/BusinessConstant.java
+++ src/main/java/fr/insee/era/extraction_rp_famille/model/BusinessConstant.java
@@ -2,6 +2,9 @@ package fr.insee.era.extraction_rp_famille.model;
 
 public final class BusinessConstant {
 
+    private BusinessConstant() {
+        throw new IllegalStateException("Utility class");
+    }
     public static final int MAX_SURVEYED_PERSONS = 10;
     public static final int MAX_CHILDREN_PER_PERSON = 8;
 }
